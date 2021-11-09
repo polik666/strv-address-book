6ab4f22a340933246f44300a21cad16845863922
Pavel Knotek <polik666@gmail.com>
2021-11-09 14:26:54 +0100

    Added token validation to a sample contacts api method

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,4 +1,24 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
+
 const router = express.Router()
+
+router.get('/', authenticateUser, (req, res) => {
+    res.json(['x', 'xx', 'xxx'])
+})
+
+function authenticateUser(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (!token) 
+        return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        console.log(err)
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 module.exports = router;
