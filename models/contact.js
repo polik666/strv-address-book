51b78fd57007d79e77e6865f9f63fcc0b46d13e2
Pavel Knotek <polik666@gmail.com>
2021-11-10 17:15:15 +0100

    Implemented basci functionality of loading and validation contacts

--- models/contact.js
+++ models/contact.js
@@ -1,5 +1,17 @@
 class Contact {
-    
+    lastName
+    firstName
+    phone
+    address
+    owner
+
+    constructor(lastName, firstName, phone, address, owner) {
+        this.lastName = lastName
+        this.firstName = firstName
+        this.phone = phone
+        this.address = address
+        this.owner = owner
+    }
 }
 
 module.exports = Contact
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,9 +2,11 @@ const express = require('express')
 const jwt = require('jsonwebtoken')
 
 const router = express.Router()
+const Contact = require('../models/contact')
+const dataLayer =  require('../data-layer/data-layer-provider').getDataLayer();
 
-router.get('/', authenticateUser, (req, res) => {
-    res.json(['x', 'xx', 'xxx'])
+router.get('/create', authenticateUser, processContactData, (req, res) => {
+    res.json(res.contact)
 })
 
 function authenticateUser(req, res, next) {
@@ -19,6 +21,17 @@ function authenticateUser(req, res, next) {
         req.user = user
         next()
     })
+}
+
+async function processContactData(req, res, next) {
+    if(!req.body.lastName){
+         return res.status(400).json({message: 'Missing required fields', fields: ['LastName']})
+    }
+    const user = await dataLayer.getUserByEmail(req.user)
+    var contact = new Contact(req.body.lastName, req.body.firstName, req.body.phone, req.body.address, user.email)
+    res.contact = contact;
+    
+    next()
 }
 
 module.exports = router;
