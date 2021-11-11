ab18d6d2684b3adaa1b58cc4f35c8f3dea8619e5
Pavel Knotek <polik666@gmail.com>
2021-11-11 16:44:22 +0100

    Test commit to verify the CI

--- routes/account.js
+++ routes/account.js
@@ -8,6 +8,10 @@ const User = require('../models/user')
 const RefreshToken = require('../models/refresh-token')
 const dataLayer =  require('../data-layer/data-layer-provider').getDataLayer();
 
+router.get('/echo', (req, res) => {
+    res.send('Hello worlds')
+})
+
 router.post('/register', processUserData, async (req, res) =>  {
     try {
         if(await dataLayer.userExists(req.user.email)) {
