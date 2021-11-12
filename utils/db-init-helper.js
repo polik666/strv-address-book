af60d8059c5106dd0e9d6e50fbd0e54a4bc09d61
Pavel Knotek <polik666@gmail.com>
2021-11-12 15:17:15 +0100

    Moved fb key to env variables

--- utils/db-init-helper.js
+++ utils/db-init-helper.js
@@ -8,11 +8,20 @@ function initDatabases() {
 
     // initialize firebase
     const admin = require("firebase-admin")
-    const serviceAccount = require(process.env.FIREBASE_CERT_PATH)
-
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: process.env.DB_FIREBASE_URL,
+        credential: admin.credential.cert({
+            type: process.env.FIREBASE_CERT_TYPE,
+            project_id: process.env.FIREBASE_CERT_PROJECT_ID,
+            private_key_id: process.env.FIREBASE_CERT_PRIVATE_KEY_ID,
+            private_key: process.env.FIREBASE_CERT_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+            client_email: process.env.FIREBASE_CERT_CLIENT_EMAIL,
+            client_id: process.env.FIREBASE_CERT_CLIENT_ID,
+            auth_uri: process.env.FIREBASE_CERT_AUTH_URI,
+            token_uri: process.env.FIREBASE_CERT_TOKEN_URI,
+            auth_provider_x509_cert_url: process.env.FIREBASE_CERT_AUTH_PROVIDER_X509_CERT_URL,
+            client_x509_cert_url: process.env.FIREBASE_CERT_CLIENT_X509_CERT_URL,
+        }),
+        databaseURL: process.env.DB_FIREBASE_URL
     })
 
     console.log("Firebase initialized")
